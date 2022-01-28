fbeb5c7bd64569b1016df0f550868dc337786f36
Juanma <60323892+juanmaorgaz@users.noreply.github.com>
2022-01-28 15:46:52 +0100

    Update todoList.py

--- src/todoList.py
+++ src/todoList.py
@@ -43,8 +43,7 @@ def get_items(dynamodb=None):
     # fetch todo from the database
     result = table.scan()
     return result['Items']
-#    items=result['Items']
-#    return items
+
 
 def put_item(text, dynamodb=None):
     table = get_table(dynamodb)
